a1d965f09c41bc6c1e1cdaba85d41354399f51ba
KmolYuan <pyslvs@gmail.com>
2021-04-23 00:12:01 +0800

    Implement CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{clap_app, AppSettings};
+
 fn main() {
-    println!("Hello, world!");
+    let args = clap_app! {
+        ("Reveal.yaml Manager") =>
+        (version: env!("CARGO_PKG_VERSION"))
+        (author: env!("CARGO_PKG_AUTHORS"))
+        (about: env!("CARGO_PKG_DESCRIPTION"))
+        (setting: AppSettings::ArgRequiredElseHelp)
+        (@subcommand init =>
+            (alias: "new")
+            (about: "Create a new project")
+        )
+        (@subcommand serve =>
+            (about: "Serve the current project")
+            (@arg PORT: --port +takes_value "Set port")
+            (@arg DIR: -c +takes_value "Set current path")
+        )
+        (@subcommand pack =>
+            (about: "Pack the current project")
+            (@arg DIR: -c +takes_value "Set current path")
+        )
+        (@subcommand help =>
+            (about: "Serve the documentation")
+            (@arg PORT: --port +takes_value "Set port")
+        )
+    }
+    .get_matches();
+    if let Some(cmd) = args.subcommand_matches("new") {
+    } else if let Some(cmd) = args.subcommand_matches("serve") {
+    } else if let Some(cmd) = args.subcommand_matches("help") {
+    }
 }
