1b434e1ccca5e1a9e7ec7ba2b481ccd766be07ba
KmolYuan <pyslvs@gmail.com>
2021-06-08 15:02:41 +0800

    Cached mode.

--- src/serve.rs
+++ src/serve.rs
@@ -12,17 +12,27 @@ use std::{
 use temp_dir::TempDir;
 
 static ROOT: Lazy<Mutex<String>> = Lazy::new(|| Mutex::new("".to_owned()));
+static CACHE: Lazy<Mutex<String>> = Lazy::new(|| Mutex::new("".to_owned()));
 const WATERMARK: &[u8] = include_bytes!("assets/help/watermark.png");
 const ICON: &[u8] = include_bytes!("assets/help/icon.png");
 const HELP_DOC: &str = include_str!("assets/reveal.yaml");
 
+macro_rules! read_yaml {
+    () => {
+        loader(&read_to_string(ROOT.lock().unwrap().as_str())?, "/static/")?
+    };
+}
+
 /// Launch function.
-pub async fn serve<P>(port: u16, path: P, project: &str) -> Result<()>
+pub async fn serve<P>(port: u16, path: P, project: &str, cache: bool) -> Result<()>
 where
     P: AsRef<Path>,
 {
     set_current_dir(path.as_ref())?;
     ROOT.lock().unwrap().push_str(project);
+    if cache {
+        CACHE.lock().unwrap().push_str(&read_yaml!());
+    }
     let temp = match TempDir::new() {
         Ok(v) => v,
         Err(s) => return Err(Error::new(ErrorKind::InvalidData, s)),
@@ -59,11 +69,13 @@ mod site {
 
     #[get("/")]
     pub(super) async fn index() -> Result<HttpResponse> {
-        let yaml = read_to_string(ROOT.lock().unwrap().as_str())?;
-        let html = loader(&yaml, "/static/")?;
         Ok(HttpResponse::Ok()
             .content_type("text/html;charset=utf-8")
-            .body(html))
+            .body(if CACHE.lock().unwrap().is_empty() {
+                read_yaml!()
+            } else {
+                CACHE.lock().unwrap().clone()
+            }))
     }
 
     #[get("/help/")]
