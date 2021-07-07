396479351816729e29c0034e7ecabd093bd8ba43
KmolYuan <pyslvs@gmail.com>
2021-07-07 14:11:05 +0800

    Make the crate as binary type.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+macro_rules! get_archive {
+    () => {{
+        use std::env::current_exe;
+        let mut path = current_exe()?.with_file_name(ARCHIVE);
+        path.set_extension("zip");
+        path
+    }};
+}
+
+pub use crate::blank::*;
+pub use crate::fmt::*;
+pub use crate::loader::*;
+pub use crate::pack::*;
+pub use crate::serve::*;
+pub use crate::update::*;
 use clap::{clap_app, AppSettings};
-use reveal_yaml::*;
+
+mod blank;
+mod fmt;
+mod loader;
+mod pack;
+mod serve;
+mod update;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
