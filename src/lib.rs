e494db4ccd2cfdce6553fcdc671765e81d0e21a8
KmolYuan <pyslvs@gmail.com>
2021-11-05 12:00:12 +0800

    Add warning for doc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! Rust implementation of [Reveal.js](https://github.com/hakimel/reveal.js) YAML server,
 //! a command line interface (CLI) tool.
+//!
+//! *This documentation is prepared for inline-API.*
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 pub use crate::blank::{blank, ROOT};
