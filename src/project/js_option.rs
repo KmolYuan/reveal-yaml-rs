73a5a954acf39b260b18decf105439eb833f5fba
KmolYuan <pyslvs@gmail.com>
2022-03-16 16:26:05 +0800

    Improve JS codebase.

--- src/project/js_option.rs
+++ src/project/js_option.rs
@@ -39,7 +39,7 @@ impl ToHtml for JsOption {
             .into_iter()
             .map(|(k, j)| {
                 "\n".to_string()
-                    + &" ".repeat(12)
+                    + &" ".repeat(8)
                     + &lower_camelcase(&k)
                     + ": "
                     + &j.to_html(_ctx)
